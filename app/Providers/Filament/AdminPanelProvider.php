7a25e5ce8954a83311cfcaeb54adf567495479d9
arigunawanj <arigunawanjatmiko@gmail.com>
2023-12-15 16:56:23 +0700

    BANTENG

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->spa()
+            ->sidebarCollapsibleOnDesktop()
             ->default()
             ->id('admin')
             ->path('admin')
